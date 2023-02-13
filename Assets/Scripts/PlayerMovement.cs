f14035679db444b241c95a05b0443f32d11bd3cd
Filip <filip4125@gmail.com>
2023-02-13 17:07:37 +0100

    Dodata brzina igraču po horizontalnoj osi x

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -9,16 +10,20 @@ public class PlayerMovement : MonoBehaviour
     // Potrebna je promenjiva koja će imati vrednost 1 ili -1 kada igrač pritisne levo ili desno
     Vector2 moveInput;
 
+    Rigidbody2D myRigidbody;
+
+    [SerializeField] float moveSpeed;
+
     void Start()
     {
-
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-
+        Run();
     }
-    
+
     // Komponenta Player Input sadrži polje actions, koje referencira na parametar koji ima sačuvane kontrole
     void OnMove(InputValue value)
     {
@@ -26,4 +31,14 @@ public class PlayerMovement : MonoBehaviour
         moveInput = value.Get<Vector2>();
         // Debug.Log(moveInput);
     }
+
+    // Metoda za kretanje igrača
+    private void Run()
+    {
+        // Potrebno je kretanje ograničiti samo na x osu, dakle horizontalno
+        Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, myRigidbody.velocity.y);
+        // Pomeramo kruto telo
+        myRigidbody.velocity = playerVelocity;
+    }
+
 }
