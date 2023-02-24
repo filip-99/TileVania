37fe27ce95c1cecb66d9ceeb8d748974a88dfc1c
Filip <filip4125@gmail.com>
2023-02-24 11:58:53 +0100

    Kreirana funkcionalnost učitavanja nivoa

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -1,16 +1,32 @@
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelExit : MonoBehaviour
 {
-    void Start()
+    [SerializeField] float levelLoadDelay = 1f;
+
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.tag == "Player")
+        {
+            StartCoroutine(LoadNextLevel());
+        }
     }
 
-    void Update()
+    // Metoda koja pre izvršavanja ima definisano vreme čekanja
+    IEnumerator LoadNextLevel()
     {
-        
+        // WaitForSecondsRealtime vree čekanja u sekundama (realno vreme)
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        int nexSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+
+        if (nexSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nexSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nexSceneIndex);
     }
 }
