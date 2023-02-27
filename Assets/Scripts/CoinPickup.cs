0c7dab6539cf136fe3fd823be54eecc05c23ae3f
Filip <filip4125@gmail.com>
2023-02-27 13:30:29 +0100

    Kreiran UI za skor i živote igrača

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -1,16 +1,24 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
-    int coinCounter;
+    // Broj poena koji se dodaje skoru za prikupljen novčić
+    [SerializeField] int numberPoints = 5;
+
+    // Da ne dođe do greške da 2 puta prikupimo novčić potreban je prekidač
+    bool wasCollected = false;
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && !wasCollected)
         {
+            wasCollected = true;
+            FindObjectOfType<GameSession>().IncreaseScore(numberPoints);
             AudioManager.instance.PlaySFX(0);
-            coinCounter++;
+            // Još jedna sigurnost da smo pokupili novčić je da isključimo objekat
+            gameObject.SetActive(false);
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameSession : MonoBehaviour
 {
     [SerializeField] int playerLives = 3;
+    int scoreCounter;
+
+
+    [SerializeField] TextMeshProUGUI livesText;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     void Awake()
     {
         // Dobijamo broj objekta u sceni koji imaju ovu skriptu
         int numGameSessions = FindObjectsOfType<GameSession>().Length;
 
-        // U koliko više od 1 objekta ima onda poništavamo sve objekte do vrednost ne bude 1
+        // Kada prvi put učitamo scenu imaćemo 1 objekat sa ovom skriptom, koji pamti pokupljene novčiće, skor, živote...
+        // Pošto imamo ovaj objekat igre za zapamćenim vrednostima i kada poginemo ne želimo da ga resetujemo tj. da kreiramo novi. Tako da će mo novi objekat igre uništiti
         if (numGameSessions > 1)
         {
             Destroy(gameObject);
@@ -22,6 +29,14 @@ public class GameSession : MonoBehaviour
         {
             DontDestroyOnLoad(gameObject);
         }
+    }
+
+    void Start()
+    {
+        scoreCounter = 0;
+
+        livesText.text = playerLives.ToString();
+        scoreText.text = scoreCounter.ToString();
     }
 
     public void ProcessPlayerDeath()
@@ -40,13 +55,21 @@ public class GameSession : MonoBehaviour
     {
         playerLives--;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        livesText.text = playerLives.ToString();
     }
 
     private void ResetGameSession()
     {
         SceneManager.LoadScene(0);
         // Kada igrač pogine važno je da uništimo ovaj objekat sesije, jer će se kreirati novi prilikom učitavanja scene
+        // Dakle kada bude Game Over potrebno je resetovati sve vrednosti i krenuti ispočetka
         Destroy(gameObject);
+    }
+
+    public void IncreaseScore(int numberPoints)
+    {
+        scoreCounter += numberPoints;
+        scoreText.text = scoreCounter.ToString();
     }
 
 }
